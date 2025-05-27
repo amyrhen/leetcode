d9061071595915a9c598b3e97e1923933ee95a97
Amyr Henry <amyrhen@outlook.com>
2025-05-27 13:04:39 -0700

    optimize difference calculation by using arithmetic series instead of iteration.

--- problems/2894/differenceOfSums.cpp
+++ problems/2894/differenceOfSums.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int differenceOfSums(int n, int m) {
-        int num1 = 0, num2 = 0;
-        for (int i = 1; i <= n; ++i) i % m > 0 ? num1 += i : num2 += i;
-        return num1 - num2;
+        int k = n / m;
+        int t = n * (n + 1) / 2;
+        return t - m * k * (k + 1);
     }
 };
